5c2568e0c43170920670b0b0a4abab913e5aedcd
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2026-09-17 22:31:02 +0100

    Add: anagram-groups - submission-17

---
+++ Data Structures & Algorithms/anagram-groups/submission-17.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        m = {}
+
+        for s in  strs:
+            alph = [0] * 26
+
+            for letter in s.strip().lower():
+                order = ord(letter) - ord('a')
+                alph[order] = alph[order] + 1      
+
+            signature = tuple(alph)
+            m[signature] = m.get(signature, []) + [s]
+
+        
+        res = []
+
+        for occur in m.values():
+            res.append(occur)
+
+        return res
